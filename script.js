a090dd15f4d0e5d1412b9ba78279535367c755b2
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-11 15:15:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function validEmail(str) {
   //your JS code here.
+	const emailPattern = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z]{2,3})+$/
+	return emailPattern.test(str);
 }
 
 // Do not change the code below.
